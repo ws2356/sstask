f09184c50bac332ccbd498ad6d545395d673618f
wansong <betterofsong@gmail.com>
2017-07-29 00:10:02 +0800

    add more cases, iam assured now

--- __tests__/test_task_scheduler.js
+++ __tests__/test_task_scheduler.js
@@ -518,4 +518,163 @@ describe('test task scheduler', function () {
         expect(results.task7).toBe(RESULT7);
       });
     });
+
+    it('complex one, simulating real case in toefl app: 1, 234, 5-2, 6-2345, 7-4', function() {
+      const RESULT1 = 'mockTask1 finished!';
+      const RESULT2 = 'mockTask2 finished!';
+      const RESULT3 = 'mockTask3 finished!';
+      const RESULT4 = 'mockTask4 finished!';
+
+      const RESULT5 = 'mockTask5 finished!';
+      const RESULT6 = 'mockTask6 finished!';
+      const RESULT7 = 'mockTask7 finished!';
+
+      const INT = 1000;
+
+      expect.assertions(23);
+
+      const mockTask1 = jest.fn((deps) => {
+        return new Promise((resolve) => setTimeout(() => resolve(RESULT1), INT * Math.random()));
+      });
+
+      const mockTask2 = jest.fn((deps) => {
+        expect(deps.task1).toBe(RESULT1);
+        return new Promise((resolve) => setTimeout(() => resolve(RESULT2), INT * Math.random()));
+      });
+      const mockTask3 = jest.fn((deps) => {
+        expect(deps.task1).toBe(RESULT1);
+        return new Promise((resolve) => setTimeout(() => resolve(RESULT3), INT * Math.random()));
+      });
+      const mockTask4 = jest.fn((deps) => {
+        expect(deps.task1).toBe(RESULT1);
+        return new Promise((resolve) => setTimeout(() => resolve(RESULT4), INT * Math.random()));
+      });
+
+      const mockTask5 = jest.fn((deps) => {
+        expect(deps.task2).toBe(RESULT2);
+        return new Promise((resolve) => setTimeout(() => resolve(RESULT5), INT * Math.random()));
+      });
+
+      const mockTask6 = jest.fn((deps) => {
+        expect(deps.task2).toBe(RESULT2);
+        expect(deps.task3).toBe(RESULT3);
+        expect(deps.task4).toBe(RESULT4);
+        expect(deps.task5).toBe(RESULT5);
+        return new Promise((resolve) => setTimeout(() => resolve(RESULT6), INT * Math.random()));
+      });
+
+      const mockTask7 = jest.fn((deps) => {
+        expect(deps.task4).toBe(RESULT4);
+        return new Promise((resolve) => setTimeout(() => resolve(RESULT7), INT * Math.random()));
+      });
+
+      const ts = new Scheduler();
+      ts.addTask('task2', mockTask2, ['task1']);
+      ts.addTask('task1', mockTask1);
+      ts.addTask('task4', mockTask4, ['task1']);
+      ts.addTask('task3', mockTask3, ['task1']);
+
+      ts.addTask('task6', mockTask6, ['task5', 'task4', 'task2', 'task3']);
+      ts.addTask('task5', mockTask5, ['task2']);
+      ts.addTask('task7', mockTask7, ['task4']);
+
+      return ts.start()
+      .then((results) => {
+        expect(mockTask1.mock.calls.length).toBe(1);
+        expect(mockTask2.mock.calls.length).toBe(1);
+        expect(mockTask3.mock.calls.length).toBe(1);
+        expect(mockTask4.mock.calls.length).toBe(1);
+        expect(mockTask5.mock.calls.length).toBe(1);
+        expect(mockTask6.mock.calls.length).toBe(1);
+        expect(mockTask7.mock.calls.length).toBe(1);
+
+        expect(results.task1).toBe(RESULT1);
+        expect(results.task2).toBe(RESULT2);
+        expect(results.task3).toBe(RESULT3);
+        expect(results.task4).toBe(RESULT4);
+        expect(results.task5).toBe(RESULT5);
+        expect(results.task6).toBe(RESULT6);
+        expect(results.task7).toBe(RESULT7);
+      });
+    });
+
+    it('complex one, simulating real case in toefl app: 1, 234, 5-2, 6-2345, 7-64', function() {
+      const RESULT1 = 'mockTask1 finished!';
+      const RESULT2 = 'mockTask2 finished!';
+      const RESULT3 = 'mockTask3 finished!';
+      const RESULT4 = 'mockTask4 finished!';
+
+      const RESULT5 = 'mockTask5 finished!';
+      const RESULT6 = 'mockTask6 finished!';
+      const RESULT7 = 'mockTask7 finished!';
+
+      const INT = 1000;
+
+      expect.assertions(24);
+
+      const mockTask1 = jest.fn((deps) => {
+        return new Promise((resolve) => setTimeout(() => resolve(RESULT1), INT * Math.random()));
+      });
+
+      const mockTask2 = jest.fn((deps) => {
+        expect(deps.task1).toBe(RESULT1);
+        return new Promise((resolve) => setTimeout(() => resolve(RESULT2), INT * Math.random()));
+      });
+      const mockTask3 = jest.fn((deps) => {
+        expect(deps.task1).toBe(RESULT1);
+        return new Promise((resolve) => setTimeout(() => resolve(RESULT3), INT * Math.random()));
+      });
+      const mockTask4 = jest.fn((deps) => {
+        expect(deps.task1).toBe(RESULT1);
+        return new Promise((resolve) => setTimeout(() => resolve(RESULT4), INT * Math.random()));
+      });
+
+      const mockTask5 = jest.fn((deps) => {
+        expect(deps.task2).toBe(RESULT2);
+        return new Promise((resolve) => setTimeout(() => resolve(RESULT5), INT * Math.random()));
+      });
+
+      const mockTask6 = jest.fn((deps) => {
+        expect(deps.task2).toBe(RESULT2);
+        expect(deps.task3).toBe(RESULT3);
+        expect(deps.task4).toBe(RESULT4);
+        expect(deps.task5).toBe(RESULT5);
+        return new Promise((resolve) => setTimeout(() => resolve(RESULT6), INT * Math.random()));
+      });
+
+      const mockTask7 = jest.fn((deps) => {
+        expect(deps.task4).toBe(RESULT4);
+        expect(deps.task6).toBe(RESULT6);
+        return new Promise((resolve) => setTimeout(() => resolve(RESULT7), INT * Math.random()));
+      });
+
+      const ts = new Scheduler();
+      ts.addTask('task2', mockTask2, ['task1']);
+      ts.addTask('task1', mockTask1);
+      ts.addTask('task4', mockTask4, ['task1']);
+      ts.addTask('task3', mockTask3, ['task1']);
+
+      ts.addTask('task6', mockTask6, ['task5', 'task4', 'task2', 'task3']);
+      ts.addTask('task5', mockTask5, ['task2']);
+      ts.addTask('task7', mockTask7, ['task4', 'task6']);
+
+      return ts.start()
+      .then((results) => {
+        expect(mockTask1.mock.calls.length).toBe(1);
+        expect(mockTask2.mock.calls.length).toBe(1);
+        expect(mockTask3.mock.calls.length).toBe(1);
+        expect(mockTask4.mock.calls.length).toBe(1);
+        expect(mockTask5.mock.calls.length).toBe(1);
+        expect(mockTask6.mock.calls.length).toBe(1);
+        expect(mockTask7.mock.calls.length).toBe(1);
+
+        expect(results.task1).toBe(RESULT1);
+        expect(results.task2).toBe(RESULT2);
+        expect(results.task3).toBe(RESULT3);
+        expect(results.task4).toBe(RESULT4);
+        expect(results.task5).toBe(RESULT5);
+        expect(results.task6).toBe(RESULT6);
+        expect(results.task7).toBe(RESULT7);
+      });
+    });
 });
